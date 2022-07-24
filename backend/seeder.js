962c69a66bf1875584d3702e86dbb6e1c114ffe9
rakunik <rnayak4455@gmail.com>
2022-07-24 08:37:59 +0530

    data seeder to database added - seeder.js

---
+++ backend/seeder.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import users from './data/users.js'
+import products from './data/products.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import connecDB from './config/db.js'
+
+dotenv.config()
+connecDB()
+
+const importData = async () => {
+	try {
+		await Order.deleteMany()
+		await Product.deleteMany()
+		await User.deleteMany()
+
+		const createdUsers = await User.insertMany(users)
+		const adminUser = createdUsers[0]
+
+		const sampleProducts = products.map((product) => {
+			return { ...product, user: adminUser._id }
+		})
+		await Product.insertMany(sampleProducts)
+
+		console.log('Data inserted successfully!'.green.inverse)
+		process.exit()
+	} catch (error) {
+		console.log(`${error.message}`.red.inverse)
+		process.exit(1)
+	}
+}
+
+const destroyData = async () => {
+	try {
+		await Order.deleteMany()
+		await Product.deleteMany()
+		await User.deleteMany()
+
+		console.log('Data destroyed successfully!'.red.inverse)
+		process.exit()
+	} catch (error) {
+		console.log(`${error.message}`.red.inverse)
+		process.exit(1)
+	}
+}
+
+if (process.argv[2] === '-d') {
+	destroyData()
+} else {
+	importData()
+}
