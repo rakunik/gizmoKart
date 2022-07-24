afc3a3ece551e97e265bdbaec518675cddbedf93
rakunik <rnayak4455@gmail.com>
2022-07-24 12:03:46 +0530

    added product routes and error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -10,14 +12,10 @@ connectDB()
 
 const app = express()
 
-app.get('/api/products', (req, res) => {
-	res.send(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((p) => p._id === req.params.id)
-	res.send(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
